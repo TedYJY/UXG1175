e4c08f808ac2ee3ba7d4cb57fbbcfd91784bb7bd
Ryan Otero <notrjao11@gmail.com>
2024-07-24 16:32:27 +0800

    fixed part1

--- Assets/Scripts/WeaponScripts/MeleeWeaponTest.cs
+++ Assets/Scripts/WeaponScripts/MeleeWeaponTest.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
 using UnityEngine;
-using UnityEngine.U2D;
 
 public class MeleeWeaponTest : MonoBehaviour
 {
